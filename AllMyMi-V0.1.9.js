8c0903abf1016f7e352d4666928b398363214ad8
Pittini1 <pittini@web.de>
2020-12-25 15:37:23 +0100

    cleanup

--- AllMyMi-V0.1.9.js
+++ AllMyMi-V0.1.9.js
@@ -1,4 +1,4 @@
-const SkriptVersion = "0.1.4"; //vom 17.12.2020 / Link zu Git: https://github.com/Pittini/iobroker-nodemihome / Forum: https://forum.iobroker.net/topic/39388/vorlage-xiaomi-airpurifier-3h-u-a-inkl-token-auslesen
+const SkriptVersion = "0.1.9"; //vom 24.12.2020 / Link zu Git: https://github.com/Pittini/iobroker-nodemihome / Forum: https://forum.iobroker.net/topic/39388/vorlage-xiaomi-airpurifier-3h-u-a-inkl-token-auslesen
 
 const mihome = require('node-mihome');
 const axios = require('axios');
@@ -28,13 +28,18 @@ const logging = true; //Logging aktivieren/deaktivieren
 */
 // ######### TESTBEREICH ################
 let TargetModel = "zhimi.airpurifier.mb3"
-//getMiotData('https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:air-purifier:0000A007:zhimi-mb3:2');
+//let miotDefinition= getMiotData('https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:air-purifier:0000A007:zhimi-mb3:2');
 
 async function getMiotData(url) {
     if (logging) log("Reaching MiotUrlConstructor");
     try {
         const response = await axios.get(url, { timeout: 10000 });
         log("resp:" + JSON.stringify(response.data));
+        for (let z in Object.keys(response.data.services)) {
+            log("Keys=" + Object.keys(response.data.services[z]))
+            log(JSON.stringify(response.data.services[z]))
+        }
+
         return response.data;
     } catch (error) {
         console.error(error);
@@ -46,7 +51,6 @@ function MiotUrlConstructor(miotdevice) {
     let BaseUrl = "https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:" + miotdevice;
     return BaseUrl;
 }
-
 
 // ################ ENDE TESTBEREICH ####################
 
@@ -74,25 +78,26 @@ DefineDevice[0] = {
     info: {},
     model: "zhimi.airpurifier.mb3",// https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:air-purifier:0000A007:zhimi-mb3:2
     description: "Purifier 3H",
+    setter: { "on": "setPower()", "mode": "setMode()", "fan-level": "setFanLevel()", "alarm.alarm": "setBuzzer()", "indicator-light.brightness": "setLcdBrightness()", "physical-controls-locked.physical-controls-locked": "setChildLock()" },
     common:
-        [{ name: "power", format: "boolean", read: true, write: true, min: false, max: true },
+        [{ name: "on", format: "boolean", read: true, write: true, min: false, max: true },
         { name: "mode", format: "string", read: true, write: true, states: { "none": "=Fanlevel", "auto": "auto", "sleep": "sleep", "favorite": "favorite" } },
         { name: "fanlevel", format: "number", read: true, write: true, min: 1, max: 3 },
         { name: "buzzer", format: "boolean", read: true, write: true, min: false, max: true },
-        { name: "lcdbrightness", format: "number", read: true, write: true, min: 0, max: 2 },
-        { name: "temperature", format: "number", read: true, write: false, min: -40, max: 125, unit: "°C" },
-        { name: "humidity", format: "number", read: true, write: false, min: 0, max: 100, unit: "%" },
-        { name: "pm2_5", format: "number", read: true, write: false, min: 0, max: 600 },
-        { name: "filterremaining", format: "number", read: true, write: false, min: 0, max: 1000, unit: "d" },
-        { name: "filterlife", format: "number", read: true, write: false, min: 0, max: 100, unit: "%" },
-        { name: "filterused", format: "number", read: true, write: false, min: false, max: true, unit: "h" },
-        { name: "childlock", format: "string", read: true, write: true, min: false, max: true }]
+        { name: "indicator-light.brightness", format: "number", read: true, write: true, min: 0, max: 2 },
+        { name: "environment.temperature", format: "number", read: true, write: false, min: -40, max: 125, unit: "°C" },
+        { name: "environment.relative-humidity", format: "number", read: true, write: false, min: 0, max: 100, unit: "%" },
+        { name: "environment.pm2.5-density", format: "number", read: true, write: false, min: 0, max: 600 },
+        { name: "filter.filterremaining", format: "number", read: true, write: false, min: 0, max: 1000, unit: "d" },
+        { name: "filter.filter-life-level", format: "number", read: true, write: false, min: 0, max: 100, unit: "%" },
+        { name: "filter.filter-used-time", format: "number", read: true, write: false, min: false, max: true, unit: "h" },
+        { name: "physical-controls-locked.physical-controls-locked", format: "boolean", read: true, write: true, min: false, max: true }]
 };
-
 DefineDevice[1] = {
     info: {},
     model: "leshow.fan.ss4",// https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:fan:0000A005:leshow-ss4:1
     description: "Fan",
+    setter: {},
     common:
         [{ name: "power", format: "boolean", read: true, write: true },
         { name: "fanlevel", format: "number", read: true, write: true, min: 1, max: 100 },
@@ -105,11 +110,15 @@ DefineDevice[2] = {
     info: {},
     model: "yeelink.light.strip2",// https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:light:0000A001:yeelink-color2:1    
     description: "Yeelight Lightstrip Plus",
+    setter: {},
     common:
         [{ name: "power", format: "boolean", read: true, write: true },
-        { name: "bright", format: "number", read: true, write: true, min: 1, max: 100 },
-        { name: "rgb", format: "number", read: true, write: true, min: 1, max: 16777215 }]
+        { name: "brightness", format: "number", read: true, write: true, min: 1, max: 100 },
+        { name: "rgb", format: "number", read: true, write: true, min: 1, max: 16777215 },
+        { name: "mode", format: "number", read: true, write: true, min: 1, max: 2 },
+        { name: "ct", format: "number", read: true, write: true, min: 1700, max: 6500 }]
 };
+
 for (let x in DefineDevice) { //An alle Devicedefinitionen die generischen Datenpunkte anhängen
     DefineDevice[x].info = [
         { id: "localip", initial: "", forceCreation: false, common: { read: true, write: true, name: "Ip Adress", type: "string", role: "value", def: "" } },
@@ -139,10 +148,10 @@ function PrepareDeviceDps(did, model) {
 
 
 function PrepareGenericDps(did) {  //GenericDps
-   // if (logging) log("Reaching PrepareGenericDps(did)");
+    // if (logging) log("Reaching PrepareGenericDps(did)");
     for (let y in DefineDevice[0].info) { //Alle info propertys des models durchgehen und Var zusammensetzen
         // log("DefineDevice[0].info[y]=" + JSON.stringify(DefineDevice[0].info[y].common.name))
-        States[DpCount] = { id: praefix0 + "." + did + ".Info." + DefineDevice[0].info[y].id, common: DefineDevice[0].info[y].common }; // 
+        States[DpCount] = { id: praefix0 + "." + did + ".info." + DefineDevice[0].info[y].id, common: DefineDevice[0].info[y].common }; // 
         DpCount++;
     };
 }
@@ -163,8 +172,6 @@ function CreateStates() {
                     setObject(praefix0 + "." + AllDevicesRaw[x].did, { type: 'device', common: { name: AllDevicesRaw[x].name }, native: {} }); //DeviceChannels machen
                     // if (logging) log("AllDevicesRaw[x]=" +JSON.stringify (AllDevicesRaw[x]))
                 };
-
-
                 main();
             };
         });
@@ -174,7 +181,7 @@ function CreateStates() {
 async function main() {
     if (logging) log("Reaching main");
     await WriteGenericDpValues();
-    //CreateDevices();
+    await CreateDevices();
     //CreateDpTrigger();
 }
 
@@ -184,9 +191,9 @@ function WriteGenericDpValues() { //Alle vorhandenen generischen Werte einlesen 
     for (let x in AllDevicesRaw) { //Alle vorhandenen Xiaomi Devices durchgehen
         for (let y in DefineDevice[0].info) { //Nimm ersten Eintrag aus DefineDevices da die Generics bei allen gleich sind
             setState(praefix0 + "." + AllDevicesRaw[x].did + ".Info." + DefineDevice[0].info[y].id, AllDevicesRaw[x][DefineDevice[0].info[y].id]);
-           // log("DefineDevice[" + 0 + "].info[" + y + "]=" + JSON.stringify(DefineDevice[0].info[y]));
-           // log("" + praefix0 + "." + AllDevicesRaw[x].did + ".Info." + DefineDevice[0].info[y].id)
-           // log(AllDevicesRaw[x][DefineDevice[0].info[y].id])
+            // log("DefineDevice[" + 0 + "].info[" + y + "]=" + JSON.stringify(DefineDevice[0].info[y]));
+            // log("" + praefix0 + "." + AllDevicesRaw[x].did + ".Info." + DefineDevice[0].info[y].id)
+            // log(AllDevicesRaw[x][DefineDevice[0].info[y].id])
         };
     };
     return true;
@@ -227,16 +234,14 @@ async function Init() { //Cloudlogin und auslesen der gesamten Clouddaten
     CreateStates();
 }
 
-
 async function CreateDevices() {
     if (logging) log("Reaching CreateDevices ");
-
     let z = 0;
 
     for (let x in AllDevicesRaw) {
         for (let y in DefineDevice) {
             if (AllDevicesRaw[x].model == DefineDevice[y].model) {
-                log("Creating device for " + AllDevicesRaw[x].model)
+                log("Now creating device for " + AllDevicesRaw[x].model + " / " + AllDevicesRaw[x].did + " / " + AllDevicesRaw[x].localip + " / " + AllDevicesRaw[x].token + " / " + refresh);
 
                 device[z] = mihome.device({
                     id: AllDevicesRaw[x].did, // required, device id
@@ -245,111 +250,118 @@ async function CreateDevices() {
                     token: AllDevicesRaw[x].token, // miio-device option, device token 4ff8a96292d0451c5148142a0a851e4f
                     refresh: refresh // miio-device option, device properties refresh interval in ms
                 });
-                log("Created device"+ JSON.stringify(device[z])+" now fetching data");
-
-                if (AllDevicesRaw[x].model == "zhimi.airpurifier.mb3") {
-                    device[z].on('properties', (data) => {
-                        for (let i in AllDevicesRaw[x].common.name) {
-                            //device[z][AllDevicesRaw[x].common.name[i]]=
-                        };
-                        log("Data=" + JSON.stringify(data))
-                        log("Keys=" + JSON.stringify(device[z].keys()))
-                        device[z].Power = device[z].getPower(); // liefert ein bestimmtes Attribut
-                        device[z].Mode = device[z].getMode(); // liefert ein bestimmtes Attribut - auto/sleep/none
-                        device[z].FanLevel = device[z].getFanLevel(); // liefert ein bestimmtes Attribut
-                        device[z].Temperature = device[z].getTemperature(); // liefert ein bestimmtes Attribut
-                        device[z].Humidity = device[z].getHumidity(); // liefert ein bestimmtes Attribut
-                        device[z].PM2_5 = device[z].getPM2_5(); // liefert ein bestimmtes Attribut
-                        device[z].FilterRemaining = device[z].getFilterRemaining(); // liefert ein bestimmtes Attribut
-                        device[z].Buzzer = device[z].getBuzzer(); // liefert ein bestimmtes Attribut
-                        device[z].LcdBrightness = device[z].getLcdBrightness(); // liefert ein bestimmtes Attribut
-                        device[z].Filterlife = device[z].getFilterlife(); // liefert ein bestimmtes Attribut
-                        device[z].Filterused = device[z].getFilterused(); // liefert ein bestimmtes Attribut
-                        RefreshDps(device[z].id, z);
-                    });
-                };
-
+                device[z].model = AllDevicesRaw[x].model;
+                device[z].setter = DefineDevice[y].setter;
+                device[z].olddata = {};
+                device[z].definitionindex = y;
+                log("Created device " + JSON.stringify(device[z]) + " now fetching data");
                 await device[z].init(); // connect to device and poll for properties
+                log("Init Device# " + z + " - device=" + JSON.stringify(device[z].model));
 
                 z++;
             };
         };
     };
 
+    for (let i in device) {
+        log("Setting trigger #" + i + " for " + device[i].model);
+        device[i].on('properties', (data) => {
+            device[i].data = data;
+            if (JSON.stringify(device[i].data) !== JSON.stringify(device[i].olddata)) {//Nur refreshen bei Datenänderung
+                RefreshDps(i);
+            };
+            device[i].olddata = device[i].data;
+        });
+    };
 
 
 
     onStop(function () { //Bei Scriptende Device löschen
         for (let x in device) {
             device[x].destroy();
-        }
+        };
         unsubscribe('properties');
     }, 10);
 }
 
-function RefreshDps(did, DeviceIndex) {
+
+function CorrectChannelId(cid) {
+
+    let temp = JSON.stringify(cid).replace(".", "") //Erstmal alle potentiellen Punkte im channelnamen entfernen
+    cid=temp.replace(/:/g, ".")
+    return cid //Jetzt alle Doppelpunkte durch Punkte ersetzen
+}
+
+function RefreshDps(DeviceIndex) {
     if (logging) log("Reaching RefreshDps ");
-    TriggerLock = true;
+    // TriggerLock = true;
+    log("Model=" + device[DeviceIndex].model + ".   Data=" + JSON.stringify(device[DeviceIndex].data) + " z=" + DeviceIndex, "warn")
+    log("Model=" + device[DeviceIndex].model + " OldData=" + JSON.stringify(device[DeviceIndex].olddata) + " z=" + DeviceIndex, "warn")
+    // log(device[DeviceIndex].model + " Power=" + device[DeviceIndex].getPower())
+    // log(Object.keys(device[DeviceIndex].data))
+    // log(device[DeviceIndex].data['environment:temperature'])
+
+    log(device[DeviceIndex].id)
+
+    for (let x in device[DeviceIndex].data) {
 
 
-    for (let x in device[DeviceIndex]) {
-        if (did == device[x].id) {
+        //   setState(praefix0 + "." + device[DeviceIndex].id + "." + DeviceGets[0], device[DeviceIndex].data[x]);
+        log(CorrectChannelId(x) + " - " + device[DeviceIndex].data[x])
 
-        }
-    }
+    };
+    return true;
 
 
-    if (device[x].Power != DeviceData[0]) {
-        DeviceData[0] = device.Power;
-        setState(praefix0 + "." + did + "." + DeviceGets[0], DeviceData[0]);
-    }
-    if (device.Mode != DeviceData[1]) {
-        DeviceData[1] = device.Mode;
-        setState(praefix0 + "." + did + "." + DeviceGets[1], DeviceData[1]);
-    }
-    if (device.FanLevel != DeviceData[2]) {
-        DeviceData[2] = device.FanLevel;
-        setState(praefix0 + "." + did + "." + DeviceGets[2], DeviceData[2]);
-    }
-    if (device.Buzzer != DeviceData[3]) {
-        DeviceData[3] = device.Buzzer;
-        setState(praefix0 + "." + did + "." + DeviceGets[3], DeviceData[3]);
-    }
-    if (device.LcdBrightness != DeviceData[4]) {
-        DeviceData[4] = device.LcdBrightness;
-        setState(praefix0 + "." + did + "." + DeviceGets[4], DeviceData[4]);
-    }
-
-    if (device.Temperature != DeviceData[5]) {
-        DeviceData[5] = device.Temperature;
-        setState(praefix0 + "." + did + "." + DeviceGets[5], DeviceData[5]);
-    }
-    if (device.Humidity != DeviceData[6]) {
-        DeviceData[6] = device.Humidity;
-        setState(praefix0 + "." + did + "." + DeviceGets[6], DeviceData[6]);
-    }
-    if (device.PM2_5 != DeviceData[7]) {
-        DeviceData[7] = device.PM2_5;
-        setState(praefix0 + "." + did + "." + DeviceGets[7], DeviceData[7]);
-    }
-    if (device.FilterRemaining != DeviceData[8]) {
-        DeviceData[8] = device.FilterRemaining;
-        setState(praefix0 + "." + did + "." + DeviceGets[8], DeviceData[8]);
-    }
-
-    if (device.Filterlife != DeviceData[9]) {
-        DeviceData[9] = device.Filterlife;
-        setState(praefix0 + "." + did + "." + DeviceGets[9], DeviceData[9]);
-    }
-    if (device.Filterused != DeviceData[10]) {
-        DeviceData[10] = device.Filterused;
-        setState(praefix0 + "." + did + "." + DeviceGets[10], DeviceData[10]);
-    }
-
-    TriggerLock = false;
-
-    if (logging) log(DeviceData)
-
+    /*
+  
+      if (device[DeviceIndex].Mode != DeviceData[1]) {
+          DeviceData[1] = device.Mode;
+          setState(praefix0 + "." + did + "." + DeviceGets[1], DeviceData[1]);
+      }
+      if (device[DeviceIndex].FanLevel != DeviceData[2]) {
+          DeviceData[2] = device.FanLevel;
+          setState(praefix0 + "." + did + "." + DeviceGets[2], DeviceData[2]);
+      }
+      if (device[DeviceIndex].Buzzer != DeviceData[3]) {
+          DeviceData[3] = device.Buzzer;
+          setState(praefix0 + "." + did + "." + DeviceGets[3], DeviceData[3]);
+      }
+      if (device[DeviceIndex].LcdBrightness != DeviceData[4]) {
+          DeviceData[4] = device.LcdBrightness;
+          setState(praefix0 + "." + did + "." + DeviceGets[4], DeviceData[4]);
+      }
+  
+      if (device[DeviceIndex].Temperature != DeviceData[5]) {
+          DeviceData[5] = device.Temperature;
+          setState(praefix0 + "." + did + "." + DeviceGets[5], DeviceData[5]);
+      }
+      if (device[DeviceIndex].Humidity != DeviceData[6]) {
+          DeviceData[6] = device.Humidity;
+          setState(praefix0 + "." + did + "." + DeviceGets[6], DeviceData[6]);
+      }
+      if (device[DeviceIndex].PM2_5 != DeviceData[7]) {
+          DeviceData[7] = device.PM2_5;
+          setState(praefix0 + "." + did + "." + DeviceGets[7], DeviceData[7]);
+      }
+      if (device[DeviceIndex].FilterRemaining != DeviceData[8]) {
+          DeviceData[8] = device.FilterRemaining;
+          setState(praefix0 + "." + did + "." + DeviceGets[8], DeviceData[8]);
+      }
+  
+      if (device[DeviceIndex].Filterlife != DeviceData[9]) {
+          DeviceData[9] = device.Filterlife;
+          setState(praefix0 + "." + did + "." + DeviceGets[9], DeviceData[9]);
+      }
+      if (device[DeviceIndex].Filterused != DeviceData[10]) {
+          DeviceData[10] = device.Filterused;
+          setState(praefix0 + "." + did + "." + DeviceGets[10], DeviceData[10]);
+      }
+  
+      TriggerLock = false;
+  
+      if (logging) log(DeviceData[DeviceIndex])
+  */
 
 }
 
