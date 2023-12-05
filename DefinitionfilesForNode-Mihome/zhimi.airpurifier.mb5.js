5e4d52d02304aad54f23e6bc47e4c7e6aa3ba22d
Black-Thunder <glwars@aol.de>
2023-12-05 13:16:38 +0100

    - added support for Air Purifier 4 (zhimi.airpurifier.mb5) - added aqi-updata-heartbeat

--- DefinitionfilesForNode-Mihome/zhimi.airpurifier.mb5.js
+++ DefinitionfilesForNode-Mihome/zhimi.airpurifier.mb5.js
@@ -1,32 +1,30 @@
 const Device = require('../device-miio');
-//edited and checked for full funcionality by Wildbill-Z
+
 module.exports = class extends Device {
 
-  static model = 'zhimi.airpurifier.mb3';
-  static name = 'Mi Air Purifier 3H';
+  static model = 'zhimi.airpurifier.mb5';
+  static name = 'Mi Air Purifier 4';
   static image = 'https://cdn.awsde0.fds.api.mi-img.com/miio.files/developer_15504816557tej1pj6.png';
 
   constructor(opts) {
     super(opts);
 
-    this._miotSpecType = 'urn:miot-spec-v2:device:air-purifier:0000A007:zhimi-mb3:2';
+    this._miotSpecType = 'urn:miot-spec-v2:device:air-purifier:0000A007:zhimi-mb5:1';
     this._propertiesToMonitor = [
       'air-purifier:fault',
       'air-purifier:on',
       'air-purifier:fan-level',
       'air-purifier:mode',
+      'air-purifier:anion',
       'environment:pm2.5-density',
       'environment:relative-humidity',
       'environment:temperature',
       'filter:filter-life-level',
       'filter:filter-used-time',
+      'filter:filter-left-time',
       'alarm:alarm',
-      'indicator-light:brightness',
-      'indicator-light:on',
-      'motor-speed:motor-speed',
-      'motor-speed:motor-set-speed',
-      'motor-speed:favorite-fan-level',
-      'use-time:use-time',
+      'custom-service:favorite-level',
+      'physical-controls-locked:brightness',
       'physical-controls-locked:physical-controls-locked',
       'aqi:aqi-updata-heartbeat'];
   }
@@ -40,24 +38,34 @@ module.exports = class extends Device {
     if (mode === 0) return 'auto';
     if (mode === 1) return 'sleep';
     if (mode === 2) return 'favorite';
-    if (mode === 3) return 'none';
+    if (mode === 3) return 'manual';
+    return undefined;
+  }
+
+  getFault() {
+    const fault = this.properties['air-purifier:fault'];
+    if (fault === 0) return 'No Faults"';
+    if (fault === 1) return 'Sensor PM Error';
+    if (fault === 2) return 'Temp Error';
+    if (fault === 3) return 'Hum Error';
+    if (fault === 4) return 'No Filter';
     return undefined;
   }
 
   getFanLevel() { // 1 - 3
-    return this.properties['air-purifier:fan-level'];
+    const fanLevel = this.properties['air-purifier:fan-level'];
+    if (fanLevel === 1) return 'Level1';
+    if (fanLevel === 2) return 'Level2';
+    if (fanLevel === 3) return 'Level3';
+    return undefined;
   }
 
-  getFavLevel() { // 1 - 9
-    return this.properties['motor-speed:favorite-fan-level'];
+  getFavLevel() { // 0 - 11
+    return this.properties['custom-service:favorite-level'];
   }
 
-  getSpeed() {
-    return this.properties['motor-speed:motor-speed'];
-  }
-
-  getSetSpeed() {
-    return this.properties['motor-speed:motor-set-speed'];
+  getAnion() {
+    return this.properties['air-purifier:anion'];
   }
 
   getTemperature() {
@@ -74,24 +82,15 @@ module.exports = class extends Device {
   }
 
   getFilterRemaining() {
-    const filterTotal = this.properties['filter:filter-life-level'];
-    const filterUsed = this.properties['filter:filter-used-time'];
-    if (filterTotal > 0 && filterUsed >= 0) {
-      return Math.max((1 - filterUsed / filterTotal) * 100, 0);
-    }
-    return undefined;
+    return this.properties['filter:filter-left-time'];
   }
 
   getBuzzer() {
     return this.properties['alarm:alarm'];
   }
 
-  getUseTime() {
-    return this.properties['use-time:use-time'];
-  }
-
   getLcdBrightness() {
-    return this.properties['indicator-light:brightness'];
+    return this.properties['physical-controls-locked:brightness'];
   }
 
   getAqiUpateInterval() {
@@ -110,24 +109,31 @@ module.exports = class extends Device {
     return this.miotSetProperty('air-purifier:mode', v);
   }
 
-  setFavSpeed(v) { // 300-2200
+  setFavSpeed(v) { // 200-2300
     return this.miotSetProperty('custom-service:favorite-speed', v);
   }
 
   setFanLevel(v) { // 1-3
+    if (v === 'Level1') v = 1;
+    else if (v === 'Level2') v = 2;
+    else if (v === 'Level3') v = 3;
     return this.miotSetProperty('air-purifier:fan-level', v);
   }
 
-  setFavLevel(v) { // 1 - 9
-    return this.miotSetProperty('motor-speed:favorite-fan-level', v);
+  setFavLevel(v) { // 0 - 11
+    return this.miotSetProperty('custom-service:favorite-level', v);
+  }
+
+  setAnion(v) {
+    return this.miotSetProperty('air-purifier:anion', v);
   }
 
   setBuzzer(v) {
     return this.miotSetProperty('alarm:alarm', v);
   }
 
-  setLcdBrightness(v) { // 0-brightest, 1-glimmer, 2-led_closed
-    return this.miotSetProperty('indicator-light:brightness', v);
+  setLcdBrightness(v) { // 0-Close, 1-Bright, 2-Brightest
+    return this.miotSetProperty('physical-controls-locked:brightness', v);
   }
 
   setChildLock(v) {
